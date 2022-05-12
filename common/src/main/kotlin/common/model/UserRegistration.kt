c1bed63ad8ba42029b076a887ea3d43b0e7cf977
yogeshvu <yogesh.d.barve@vanderbilt.edu>
2022-05-12 10:31:31 -0500

    This addresses issue #8

--- common/src/main/kotlin/common/model/UserRegistration.kt
+++ common/src/main/kotlin/common/model/UserRegistration.kt
@@ -5,7 +5,8 @@ import com.fasterxml.jackson.annotation.JsonInclude
 @JsonInclude(JsonInclude.Include.NON_EMPTY)
 data class UserRegistration(
     val isAdministrator: Boolean,
-    val isRegistered: Int,
+    val isRegistered: String,
     val isRegistrationApprover: Boolean,
-    val userId: String
+    val userId: String,
+    val permission: String,
 )
