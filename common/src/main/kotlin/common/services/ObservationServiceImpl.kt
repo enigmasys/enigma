346ec35aec8809280067370ec20e6e74fca1d58d
yogeshVU <yogeshVU@users.noreply.github.com>
2022-08-29 11:31:03 -0700

    update for the #25 (#26)

--- common/src/main/kotlin/common/services/ObservationServiceImpl.kt
+++ common/src/main/kotlin/common/services/ObservationServiceImpl.kt
@@ -4,17 +4,22 @@ import com.azure.storage.blob.BlobClientBuilder
 import common.model.Directory
 import common.model.TransferStat
 import common.model.observation.EgressResult
+import common.model.observation.PeekObservationResult
 import common.model.observation.UploadObservationObject
 import common.services.auth.AuthService
 import org.slf4j.LoggerFactory
+import org.springframework.http.HttpStatus
 import org.springframework.stereotype.Service
 import org.springframework.web.reactive.function.BodyInserters
 import org.springframework.web.reactive.function.client.WebClient
 import org.springframework.web.util.UriBuilder
 import reactor.core.publisher.Mono
 import java.io.OutputStream
+import java.lang.Integer.max
+import java.lang.Integer.min
 import java.nio.file.Files
 import java.nio.file.Paths
+import kotlin.system.exitProcess
 
 @Service
 class ObservationServiceImpl(
@@ -323,5 +328,77 @@ class ObservationServiceImpl(
 
             FileDownloader.get(url, filePath)
         }
+    }
+
+
+    fun getPeekObservations(
+        processID: String,
+        startObsIndex: String,
+        version: String,
+        maxReturn: Int = 500,
+        isForwardDir: Boolean = true
+    ): PeekObservationResult? {
+        val token = authService.getAuthToken()
+
+        apiVersion = "/v2"
+        val response = webClient
+            .get()
+            .uri { uriBuilder: UriBuilder ->
+                uriBuilder.path(apiVersion + "/Process/PeekObservations")
+                    .queryParam("processId", processID)
+                    .queryParam("obsIndex", startObsIndex)
+                    .queryParam("version", version)
+                    .queryParam("maxReturn",maxReturn)
+                    .queryParam("isForwardDir",isForwardDir)
+                    .build()
+            }
+            .headers { it.setBearerAuth(token) }
+            .retrieve()
+            .onStatus(HttpStatus::is4xxClientError) { Mono.error(RuntimeException("4XX Error ${it.statusCode()}, ${it.bodyToMono(String::class.java)}")) }
+            .onStatus(HttpStatus::is5xxServerError) { Mono.error(RuntimeException("5XX Error ${it.statusCode()}, ${it.bodyToMono(String::class.java)}")) }
+            .bodyToMono(PeekObservationResult::class.java)
+        val result = response.share().block()
+        return result
+    }
+
+    fun getAllPeekObservations(
+        processID: String,
+        endObsIndex: String,
+        version: String = "0",
+    ): PeekObservationResult? {
+        var tmpResult = mutableListOf<UploadObservationObject>()
+
+        if (endObsIndex.toInt() < 500) {
+            return getPeekObservations(
+                processID = processID,
+                startObsIndex = "0",
+                maxReturn = 500,
+                version = version
+            )
+        }
+        else{
+            var remainingCount = endObsIndex.toInt()
+            var maxResult = 0
+            var startNumber = 0
+            while (remainingCount>0){
+                if (remainingCount>=500) {
+                    maxResult = 500
+                    remainingCount =-500
+                }else
+                {
+                    maxResult = remainingCount
+                    remainingCount =0
+                }
+                var tmpReturn =  getPeekObservations(
+                    processID = processID,
+                    startObsIndex = startNumber.toString(),
+                    maxReturn = maxResult,
+                    version = version
+                )
+                tmpReturn?.let { tmpResult.addAll(it) }
+                startNumber += min(remainingCount, 500)
+            }
+        }
+        return tmpResult.toTypedArray() as PeekObservationResult
     }
 }
