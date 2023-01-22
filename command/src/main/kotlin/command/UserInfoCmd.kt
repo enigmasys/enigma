7c612ef4495712d94bac24a4b5487cc745fc855f
yogeshvu <yogesh.d.barve@vanderbilt.edu>
2023-01-22 12:41:49 -0600

    added login/logout functionality for device code.

--- command/src/main/kotlin/command/UserInfoCmd.kt
+++ command/src/main/kotlin/command/UserInfoCmd.kt
@@ -7,7 +7,11 @@ import common.services.UserInfo
 import common.services.auth.AuthService
 import common.util.prettyJsonPrint
 import org.springframework.stereotype.Component
+import org.springframework.util.ResourceUtils
 import picocli.CommandLine
+import java.io.File
+import java.nio.file.Files
+import java.nio.file.Paths
 import java.util.concurrent.Callable
 
 @Component
@@ -24,8 +28,11 @@ class UserInfoCmd(
     : Callable<Int>
 {
 
-    @CommandLine.Option(names = ["-s", "--status"], description = ["Display the Status of this Application/User."])
+    @CommandLine.Option(names = ["-s", "--status","--login"], description = ["Display the Status of this Application/User."])
     var appStatus = false
+
+    @CommandLine.Option(names = ["-o", "--logout"], description = ["Logout from this Application"])
+    var loginFun = false
 
     @CommandLine.ParentCommand
     val parent: EnigmaCommand? = null
@@ -37,6 +44,29 @@ class UserInfoCmd(
             } }
 
         when{
+
+            loginFun -> {
+                println("Login Functionality..")
+
+                val file: File = ResourceUtils.getFile(".sample_cache.json")
+
+                if(file.exists()){
+//                    println("file present ${file.absolutePath}")
+                    if (file.isFile) {
+                        val result = file.delete()
+                        when (result) {
+                            true -> println("User Logged out successful")
+                            false -> println("logout failed")
+                        }
+                    }
+
+                }
+                else {
+                    println("User is logged out already")
+                }
+
+            }
+
             appStatus ->{
                 val result =  UserInfoObj.getUserRegistration()
                 if (result != null) {
