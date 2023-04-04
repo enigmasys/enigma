67a33ac19b63150298d82634eb332d3f287e5809
Dax <dax.westerman@vumc.org>
2023-04-04 17:14:37 -0500

    Returned to jvmTarget 1.8

--- secretapp/build.gradle.kts
+++ secretapp/build.gradle.kts
@@ -103,9 +103,9 @@ publishing {
 }
 val compileKotlin: KotlinCompile by tasks
 compileKotlin.kotlinOptions {
-    jvmTarget = "11"
+    jvmTarget = "1.8"
 }
 val compileTestKotlin: KotlinCompile by tasks
 compileTestKotlin.kotlinOptions {
-    jvmTarget = "11"
+    jvmTarget = "1.8"
 }
