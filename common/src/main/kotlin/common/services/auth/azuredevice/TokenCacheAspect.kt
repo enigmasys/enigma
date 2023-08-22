a556a439d7b38b7a6ad0604ffdf6923398f6b914
yogeshvu <yogesh.d.barve@vanderbilt.edu>
2023-08-22 15:18:33 -0500

    addresses the #58 supporting encryption/decryption of the stored device code.

--- common/src/main/kotlin/common/services/auth/azuredevice/TokenCacheAspect.kt
+++ common/src/main/kotlin/common/services/auth/azuredevice/TokenCacheAspect.kt
@@ -6,26 +6,21 @@ import org.springframework.util.ResourceUtils
 import java.io.File
 import java.nio.file.Files
 import java.nio.file.Paths
-import javax.crypto.KeyGenerator
-import javax.crypto.SecretKey
 
 
-
-
-// Copyright (c) Microsoft Corporation. All rights reserved.
-// Licensed under the MIT License.
 class TokenCacheAspect(fileName: String)
  : ITokenCacheAccessAspect {
     private var data: String
     private var cacheFileName:String
-
-    private lateinit var fileEncrypterDecrypter: CryptoFile
     init {
-
-        data = readDataFromFile(fileName)
+        // surround with try catch
+        try {
+            data = CryptHelper.decrypt("Bar12345Bar12346", readDataFromFile(fileName))!!
+        }catch (e: Exception) {
+            println("Error decrypting the data from the file: $fileName")
+            data = ""
+        }
         cacheFileName = fileName
-//        val secretKey: SecretKey = KeyGenerator.getInstance("AES").generateKey()
-//        val fileEncrypterDecrypter = CryptoFile(secretKey, "AES/CBC/PKCS5Padding")
     }
 
     override fun beforeCacheAccess(iTokenCacheAccessContext: ITokenCacheAccessContext) {
@@ -35,19 +30,18 @@ class TokenCacheAspect(fileName: String)
     override fun afterCacheAccess(iTokenCacheAccessContext: ITokenCacheAccessContext) {
         data = iTokenCacheAccessContext.tokenCache().serialize()
         var file = File(cacheFileName)
-        file.writeText(data)
-//        fileEncrypterDecrypter.encrypt(data, "baz.enc");
 
-        // you could implement logic here to write changes to file
+        val encryptData = CryptHelper.encrypt("Bar12345Bar12346", data)
+        file.writeText(encryptData!!)
     }
 
     companion object {
         private fun readDataFromFile(resource: String): String {
             return try {
-                //Determine if sample running from IDE (resource URI starts with 'file') or from a .jar (resource URI starts with 'jar'),
+                //Determine if sample running from IDE (resource URI starts with 'file')
+                // or from a .jar (resource URI starts with 'jar'),
                 //  so that sample_cache.json is read properly
                 val file: File = ResourceUtils.getFile(resource)
-
                 if(file.exists()){
                     return String(
                         Files.readAllBytes(
@@ -56,18 +50,7 @@ class TokenCacheAspect(fileName: String)
                     )
                 }
                 else {
-//                    val uri = TokenCacheAspect::class.java.getResource(resource).toURI()
                     val uri = file.toURI()
-                    val env: MutableMap<String, String?> = HashMap()
-                    env["create"] = "true"
-                    var isFileCreated = file.createNewFile()
-//                    if(isFileCreated){
-//                        println("$uri is created successfully.")
-//                    } else{
-//                        println("$uri already exists.")
-//                    }
-
-//                    val fs = FileSystems.newFileSystem(uri, env)
                     val myFolderPath = Paths.get(uri)
                     return String(Files.readAllBytes(myFolderPath))
                 }
