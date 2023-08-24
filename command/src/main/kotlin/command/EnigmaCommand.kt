fa5f432fe12755ac2de680a4341de20e62dec585
yogeshvu <yogesh.d.barve@vanderbilt.edu>
2023-08-24 15:09:42 -0500

    addresses the #59 supporting adding a remote version checking for the updates...

--- command/src/main/kotlin/command/EnigmaCommand.kt
+++ command/src/main/kotlin/command/EnigmaCommand.kt
@@ -1,38 +1,24 @@
 package command
 
 
-import com.fasterxml.jackson.databind.ObjectMapper
-import com.fasterxml.jackson.module.kotlin.jacksonObjectMapper
-import com.fasterxml.jackson.module.kotlin.readValue
+import org.springframework.context.annotation.ComponentScan
+import org.springframework.context.annotation.Configuration
+import picocli.CommandLine
 import picocli.CommandLine.Command
 import picocli.CommandLine.Option
 import java.util.concurrent.Callable
 
-
-import common.model.observation.UploadObservationObject
-import common.model.testdata.MRIData
-import common.util.prettyJsonPrint
-import org.springframework.context.annotation.ComponentScan
-import org.springframework.context.annotation.Configuration
-import picocli.CommandLine
-import common.services.FileUploader
-import common.services.ObservationServiceImpl
-import common.services.ObservationUploadServiceImpl
-import common.services.PremonitionProcessServiceImpl
-import common.services.auth.AuthService
-import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty
-
-//@Component
 @Configuration
 @Command(
-    version = ["LEAP_CLI", "1.0.0"],
+    versionProvider = CLIVersionProvider::class,
     mixinStandardHelpOptions = true,
     description = ["Command for accessing the UDCP"],
 )
 @ComponentScan(basePackages = ["common","common.util"])
-class EnigmaCommand(
-) : Callable<Int> {
-    @Option(names = ["-t", "--token"], description = ["Auth Token to pass when using Auth Passthrough Mode!"], scope = CommandLine.ScopeType.INHERIT)
+class EnigmaCommand: Callable<Int> {
+    @Option(names = ["-t", "--token"],
+        description = ["Auth Token to pass when using Auth Passthrough Mode!"],
+        scope = CommandLine.ScopeType.INHERIT)
     var token:String? = null
     override fun call(): Int {
         return 0
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -6,22 +6,22 @@ include("api")
 include("client")
 
 
-val archivaHostId: String? by settings
-val archivaPort: String? by settings
-
-dependencyResolutionManagement {
-    repositories {
-        mavenLocal()
-        mavenCentral()
-        archivaPort?.let {
-            archivaHostId?.let {
-                maven {
-                    isAllowInsecureProtocol = true
-                    url = uri("http://$archivaHostId:$archivaPort/repository/snapshots")
-                }
-            }
-        }
-    }
-}
-include("client")
-include("client")
+//val archivaHostId: String? by settings
+//val archivaPort: String? by settings
+//
+//dependencyResolutionManagement {
+//    repositories {
+//        mavenLocal()
+//        mavenCentral()
+//        archivaPort?.let {
+//            archivaHostId?.let {
+//                maven {
+//                    isAllowInsecureProtocol = true
+//                    url = uri("http://$archivaHostId:$archivaPort/repository/snapshots")
+//                }
+//            }
+//        }
+//    }
+//}
+//include("client")
+//include("client")
