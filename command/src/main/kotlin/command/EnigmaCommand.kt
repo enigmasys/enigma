58bbcab43570e79af71bc8b1c014fd77d17c5262
yogeshVU <yogeshVU@users.noreply.github.com>
2024-02-06 12:42:02 -0600

    Print the help message if no argument passed #165 (#185)

--- command/src/main/kotlin/command/EnigmaCommand.kt
+++ command/src/main/kotlin/command/EnigmaCommand.kt
@@ -4,6 +4,7 @@ import org.springframework.context.annotation.ComponentScan
 import org.springframework.context.annotation.Configuration
 import picocli.CommandLine
 import picocli.CommandLine.Command
+import picocli.CommandLine.Model.CommandSpec
 import picocli.CommandLine.Option
 import java.util.concurrent.Callable
 
@@ -22,7 +23,12 @@ class EnigmaCommand : Callable<Int> {
     )
     var token: String? = null
 
+    @CommandLine.Spec
+    var spec: CommandSpec? = null
+
     override fun call(): Int {
+        //! Print usage if no subcommand is given and exit with a non-zero status
+        spec?.commandLine()?.usage(System.err)
         return 0
     }
 }
